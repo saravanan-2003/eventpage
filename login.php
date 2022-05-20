205f295d9d0f72c2bcf08d92dfc9efd4d39584ed
saravanan-2003 <87331965+saravanan-2003@users.noreply.github.com>
2022-05-20 14:46:05 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php      
+    include('connection.php');  
+    $email = $_POST['email'];  
+    $password = $_POST['password'];  
+      
+
+        $email = stripcslashes($email);  
+        $password = stripcslashes($password);  
+        $email = mysqli_real_escape_string($con, $email);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "select *from users where email = '$email' and password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>";  
+        }  
+        else{  
+            echo "<h1> Login failed. Invalid username or password.</h1>";  
+        }     
+?>
+  
